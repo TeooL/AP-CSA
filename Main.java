9db6b139cf5ff09f1e4b2f05b565e104e0f6d57c
TeooL <144486916+TeooL@users.noreply.github.com>
2023-09-11 15:21:04 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,6 +3,12 @@ public class Main
   public static void main(String[] args)
   {
     System.out.println("Hello World");
-    System.out.print("I am a beginner");
+    System.out.println("I am a beginner");
+    final int i = 4;
+    System.out.println(i);
+    double n = 1.23;
+    System.out.println(n);
+    boolean t = true;
+    System.out.println(t);
   }
 }

271e141fad6a2e3e3a7f5043df310c5f14d843ef
TeooL <144486916+TeooL@users.noreply.github.com>
2023-09-13 15:14:32 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
+import java.util.Scanner;
 public class Main
 {
   public static void main(String[] args)
   {
+    Scanner typein = new Scanner(System.in);
     System.out.println("Hello World");
     System.out.println("I am a beginner");
     final int i = 4;
@@ -15,6 +17,21 @@ public class Main
     int compound_expression_one = 5 + 6 / 3 * 4;
     double compound_expression_two = 10 - 9.6 / 3 * 2 ;
     System.out.println(compound_expression_one);
-    System.out.print(compound_expression_two);
+    try
+    {
+      System.out.println(4/0);
+    }
+    catch (ArithmeticException e)
+    {
+      System.out.println(e);
+    }
+    System.out.println(compound_expression_two);
+    System.out.println("Don't Mind This Line");
+    System.out.println("Time to add the scanner");
+    System.out.println("Ok Let's Test it out, Type your name");
+    String name = typein.nextLine();
+    System.out.println(name);
+    
+    
   }
 }
